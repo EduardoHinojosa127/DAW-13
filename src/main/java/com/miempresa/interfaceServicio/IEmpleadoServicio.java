fec553d9efd24997ca9e53ff910aed9d4065e365
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2022-11-23 21:31:34 -0500

    DAW-13

---
+++ src/main/java/com/miempresa/interfaceServicio/IEmpleadoServicio.java
@@ -0,0 +1,14 @@
+package com.miempresa.interfaceServicio;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.miempresa.modelo.*;
+
+public interface IEmpleadoServicio {
+	public List<Empleado> listar();
+	public List<Empleado> busqueda(String nombre);
+	public Optional<Empleado> listarId(int id);
+	public int guardar(Empleado p);
+	public void borrar(int id);
+}
---
+++ src/main/java/com/miempresa/interfaces/ITarea.java
@@ -0,0 +1,14 @@
+package com.miempresa.interfaces;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.miempresa.modelo.Empleado;
+import com.miempresa.modelo.Tarea;
+
+@Repository
+public interface ITarea extends CrudRepository<Tarea, Integer> {
+	
+}
